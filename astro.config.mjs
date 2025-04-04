07eac3311f67036b8d88f8a1da6ded5140e69c1c
Celia Chavez Virgen <c.chavezvirgen@gmail.com>
2025-04-04 16:16:11 -0600

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://celiachavezv.github.io',
+  integrations: [tailwind()],
+  
 });
-
-build: {
-  assets: 'assets'
-}
